29be38c0934035d07c2bcfe799f725f7e17d50f8
a3ryn <iridon.mihaela@gmail.com>
2017-12-10 19:45:56 -0600

    Comment cleanup

--- Shared.Core.Common/Logging/ILogManager.cs
+++ Shared.Core.Common/Logging/ILogManager.cs
@@ -8,12 +8,6 @@ using System;
 
 namespace Shared.Core.Common.Logging
 {
-    /*
-    This source file is under MIT License (MIT)
-    Copyright (c) 2016 Mihaela Iridon
-    https://opensource.org/licenses/MIT
-    */
-
     public interface ILogManager
     {
         ILogger GetLogger(string name);
